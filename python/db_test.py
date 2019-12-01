c5f87b295953514694852895bb17c367eafe6730
Jerry Liu <jerrylarryliu@gmail.com>
2019-12-01 11:00:14 -0800

    updated test code

--- python/db_test.py
+++ python/db_test.py
@@ -7,12 +7,12 @@ from mysql.connector import Error
 from mysql.connector import errorcode
 
 
-req = "http://192.168.1.150/read_table.php?table=20190918_DATA"
+req = "http://192.168.1.150/php/select_from_table.php?table=data"
 response = requests.get(req)
 data = response.json()["data"]
 list = []
 for item in data:
-    epoch_time = item["TIME"]
+    epoch_time = item["time"]
     time_str = datetime.datetime.utcfromtimestamp(int(epoch_time)).strftime("%Y-%m-%d %H:%M:%S")
     list.append(time_str)
     print(time_str)
@@ -30,9 +30,9 @@ print(0)
 try:
     connection = mysql.connector.connect(host='192.168.1.150', database='nova', user='zeus', password='Airlink_1')
     cursor = connection.cursor()
-    cursor.execute("USE nova")
-    cursor.execute("SHOW TABLES")
-    tables = cursor.fetchall()
+    # cursor.execute("USE nova")
+    # cursor.execute("SHOW TABLES")
+    # tables = cursor.fetchall()
 
 except mysql.connector.Error as error:
     print("Failed access table {}".format(error))
