7bcb3dd472590439cc5757aa30b8869a0314371e
Jerry Liu <jerrylarryliu@gmail.com>
2019-12-19 16:16:30 -0800

    working on frontend styling

--- frontend/js/sidebar.js
+++ frontend/js/sidebar.js
@@ -4,7 +4,12 @@ $(document).ready(function () {
     });
 
     $('#menu-toggle').on('click', function () {
-        $('#sidebar, #content','.side_container').toggleClass('active');
+        
+        
+        $('#sidebar, .side_container').toggleClass('active');
+        
+        $('.main').toggleClass('active');
+        
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
