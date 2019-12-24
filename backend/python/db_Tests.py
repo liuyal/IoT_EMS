5920d50a4aa08cbf0d875b5842700873bd2efe25
Jerry Liu <jerrylarryliu@gmail.com>
2019-12-23 16:35:21 -0800

    update files

--- backend/python/db_Tests.py
+++ backend/python/db_Tests.py
@@ -1,4 +1,4 @@
-import os, sys, time, datetime, requests, subprocess, serial, ipaddress
+import os, sys, time, datetime, requests, subprocess, serial, ipaddress, yaml
 from datetime import date, timedelta
 from random import seed
 from random import randint
@@ -7,33 +7,34 @@ import mysql.connector
 from mysql.connector import Error
 from mysql.connector import errorcode
 
-
 def sql_connector_test(ip):
     try:
-        connection = mysql.connector.connect(host=ip, database='nova', user='root', password='', auth_plugin='mysql_native_password')
+        with open("server_info.yaml", 'r') as stream:
+            try: mysql_cred = yaml.safe_load(stream)["mysql_cred"]
+            except yaml.YAMLError as exc: print(exc)
+        connection = mysql.connector.connect(host=mysql_cred["HOST"], database=mysql_cred["DATABASE"], user=mysql_cred["USER"], password=mysql_cred["PASSWORD"], auth_plugin='mysql_native_password')
         cursor = connection.cursor()
         cursor.execute("USE nova")
         cursor.execute("SHOW TABLES")
         tables = cursor.fetchall()
+        print(tables)
     except mysql.connector.Error as error:
         print("Failed access table {}".format(error))
 
-
-def request_test(ip, folder="backend/php/"):
-    req = "http://" + ip + "/" + folder + "show_tables.php"
+def request_test(ip):
+    req = "http://" + ip + "/backend/php/show_tables.php"
     response = requests.get(req)
     data = response.json()["data"]
 
-
 def time_check(ip):
-    show_tables = "http://" + ip + "/php/show_tables.php"
-    read_table = "http://" + ip + "/php/select_from_table.php?table="
+    show_tables = "http://" + ip + "/backend/php/show_tables.php"
+    read_table = "http://" + ip + "/backend/php/select_from_table.php?table="
     response = requests.get(show_tables)
     table_resp = response.json()["data"]
     tables = []
     for item in table_resp: tables.append(item['table'])
     for date in tables:
-        try:
+        if date != "nodes" and date != "system_config":
             response = requests.get(read_table + date)
             resp = response.json()["data"]
             print(date)
@@ -41,8 +42,6 @@ def time_check(ip):
                 epoch_time = item["time"]
                 times = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(int(epoch_time)))
                 print("\t" + str(times))
-        except: None
-
 
 def data_generator(ip, n=10):
     for _ in range(n):
@@ -55,16 +54,17 @@ def data_generator(ip, n=10):
         print(str(epoch) + " " + time.strftime('%Y%m%d', time.gmtime(epoch)) + " " + str(temp) + " " + str(hum) + "\n" + insert_req)
         print(response.json())
 
-
-def add_nodes(mac, ip):
+def add_nodes(mac):
     try:
-        connection = mysql.connector.connect(host=ip, database='nova', user='root', password='', auth_plugin='mysql_native_password')
+        with open("server_info.yaml", 'r') as stream:
+            try: mysql_cred = yaml.safe_load(stream)["mysql_cred"]
+            except yaml.YAMLError as exc: print(exc)
+        connection = mysql.connector.connect(host=mysql_cred["HOST"], database=mysql_cred["DATABASE"], user=mysql_cred["USER"], password=mysql_cred["PASSWORD"], auth_plugin='mysql_native_password')
         cursor = connection.cursor()
         cursor.execute("USE nova")
         for item in mac:
             cursor.execute("INSERT INTO nodes values('" + item + "', '0.0.0.0', 9996, 0, FALSE)")
             connection.commit()
-
     except mysql.connector.Error as error:
         print("Failed access table {}".format(error))
 
@@ -75,9 +75,9 @@ if __name__ == "__main__":
     port = 9996
     mac = ["BC:DD:C2:2F:47:79", "5C:CF:7F:AC:72:78"]
 
-    # add_nodes(mac, ip)
-    data_generator(ip , 20)
-    # time_check()
+    # data_generator(ip , 20)
+    # add_nodes(mac)
+    time_check(ip)
 
 
 
