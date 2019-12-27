de110a4e46233a24b24845961871f400941c785d
Jerry Liu <jerrylarryliu@gmail.com>
2019-12-27 11:08:34 -0800

    'working on frontend'

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -16,35 +16,40 @@ function RequestData(url, callback) {
 
 
 function load_fresh() {
-
     window.setInterval(function () {
         var time = new Date();
         var h = ("0" + time.getUTCHours()).slice(-2);
         var m = ("0" + time.getUTCMinutes()).slice(-2);
         var s = ("0" + time.getUTCSeconds()).slice(-2);
         var hms = h + ":" + m + ":" + s;
-
         const url = "http://localhost/Temperature_System/backend/php/get_fresh_data.php";
-
         RequestData(url, function (json) {
             var data = json;
             var online = data["message"]["online"];
             var temp = data["message"]["temp"];
             var hum = data["message"]["hum"];
-
             document.getElementById('n_online').innerHTML = online;
             document.getElementById('temp').innerHTML = temp + "&ordm;C";
             document.getElementById('hum').innerHTML = hum + "%";
             document.getElementById('current_time').innerHTML = hms;
-
             if (online == 0) {
                 document.getElementById('wifi_on').innerHTML = "wifi_off";
             } else {
                 document.getElementById('wifi_on').innerHTML = "wifi";
             }
-
         });
-        document.getElementById('wifi').innerHTML = hms;
     }, 1000);
+}
 
+
+function load_graph_data() {
+
+    const url = "http://localhost/Temperature_System/backend/php/select_from_table.php?table=data";
+    RequestData(url, function (json) {
+        var data = json["data"];
+        
+        console.log(data)
+
+
+    });
 }
