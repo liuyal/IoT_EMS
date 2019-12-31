d17e917aa1ecf9dd5efb7eefff76595b1689978a
Jerry Liu <jerrylarryliu@gmail.com>
2019-12-31 13:45:18 -0800

    'check in'

--- backend/python/Test_Code/sudo_cron.py
+++ backend/python/Test_Code/sudo_cron.py
@@ -6,6 +6,6 @@ while True:
     print(now.split(".")[0])
     if now.split(".")[0].split(":")[2] == "00":
         print("RUN")
-        os.system("python ../udp_cmd.py -f -v -L 9996")
+        os.system("python ../udp_cmd.py -f -v -u -L 9996")
     time.sleep(1)
 
